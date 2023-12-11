768ec69b8c20769cd9f4ba8b27b734c72c2f34c1
Ian <47256454+IanRFerguson@users.noreply.github.com>
2023-12-10 21:21:57 -0500

    wip

--- frontend/src/utils/chart.js
+++ frontend/src/utils/chart.js
@@ -6,7 +6,6 @@ export function clearChart() {
 }
 
 function setHeader(data, average) {
-    console.log(data);
     var div = document.getElementById("teamChart");
     div.innerHTML += `<h2>${data.full_name}</h2>`;
     div.innerHTML += `<p>Season average: ${average} points</p>`
@@ -55,6 +54,36 @@ export function drawChart(data) {
     svg.append("g")
         .call(d3.axisLeft(y));
 
+    // Tooltip
+    var tooltip = d3.select("#teamChart")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+
+    var mouseover = function(d) {
+        tooltip
+        .style("opacity", 1)
+    }
+
+    var mousemove = function(d) {
+        tooltip
+        .html("Opponent: " + d.opponent)
+        .style("left", (d3.pointer(this)[0]+90) + "px") 
+        .style("top", (d3.pointer(this)[1]) + "px")
+    }
+
+    var mouseleave = function(d) {
+        tooltip
+        .transition()
+        .duration(200)
+        .style("opacity", 0)
+    }
+
     // Line it up!
     svg.append("path")
         .datum(stats)
@@ -64,5 +93,8 @@ export function drawChart(data) {
         .attr("d", d3.line()
             .x(function (d) { return x(Date.parse(d.date)) })
             .y(function (d) { return y(parseInt(d.points)) })
-        );
+        )
+        .on("mouseover", mouseover )
+        .on("mousemove", mousemove )
+        .on("mouseleave", mouseleave ) 
 }
